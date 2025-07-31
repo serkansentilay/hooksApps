f7880ce273d182a21532a8c54b903777264102cc
serkansentilay <sentiilay@gmail.com>
2025-07-31 14:21:10 +0300

    app js file type converted

--- App.js
+++ App.js
@@ -4,11 +4,17 @@
  *
  * @format
  */
-
+import React, {useState, useEffect} from 'react'
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 
+console.log("1 kez calisirm ben")
 function App() {
+  const [age, setAge] = useState(42);
+  const [fruit, setFruit] = useState('banana');
+  const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
+  //usestate ifade degistirmek icin ve birde baslangic degeri alir ilk renderda kullanir
+  console.log(`keyfime gore`)
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
